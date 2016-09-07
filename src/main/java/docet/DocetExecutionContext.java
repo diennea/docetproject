54bc51919876e3c24d9982f6b92b7a49c96558f4
Matteo Casadei <matteo.casadei@diennea.com>
2016-09-07 07:14:13 +0000

    MN-10854. First version of execution and security context in effect.

---
+++ src/main/java/docet/DocetExecutionContext.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to Diennea S.r.l. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Diennea S.r.l. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package docet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Represents Docet's execution context (at the request level).
+ *
+ */
+public class DocetExecutionContext {
+
+    private final HttpServletRequest executionRequest;
+    private final Set<String> permittedPackages;
+
+    public HttpServletRequest getExecutionRequest() {
+        return executionRequest;
+    }
+
+    public DocetExecutionContext(final HttpServletRequest req) {
+        this.executionRequest = req;
+        this.permittedPackages = new HashSet<>();
+    }
+
+    public void setAccessPermission(final String packageid, final AccessPermission access) {
+        switch (access) {
+            case ALLOW:
+                this.permittedPackages.add(packageid);
+                break;
+            case DISALLOW:
+                this.permittedPackages.remove(packageid);
+                break;
+            default: new RuntimeException("Value " + access + " is not supported");
+        }
+    }
+
+    public boolean checkPermission(final String packageid, final AccessPermission access) {
+        boolean res = false;
+        switch (access) {
+            case ALLOW:
+                res = this.permittedPackages.contains(packageid);
+                break;
+            case DISALLOW:
+                res = !this.permittedPackages.contains(packageid);
+                break;
+            default: new RuntimeException("Access level " + access + " is not supported");
+        }
+        return res;
+    }
+
+    public static enum AccessPermission {
+        ALLOW,
+        DISALLOW
+    }
+}
---
+++ src/main/java/docet/error/DocetPackageException.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to Diennea S.r.l. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Diennea S.r.l. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package docet.error;
+
+public class DocetPackageException extends Exception {
+
+    public static final String MSG_NOT_FOUND = "package_not_found";
+    public static final String MSG_ACCESS_DENIED = "package_access_denied";
+
+    private DocetPackageException(String message) {
+        super(message);
+    }
+    
+    private DocetPackageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public static DocetPackageException buildPackageAccessDeniedException() {
+        return new DocetPackageException(MSG_ACCESS_DENIED);
+    }
+
+    public static DocetPackageException buildPackageNotFoundException(Throwable cause) {
+        return new DocetPackageException(MSG_NOT_FOUND, cause);
+    }
+}
